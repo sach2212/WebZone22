f44871b9ec55a019214d59a1702e6785153a5bfb
sach2212 <121556074+sach2212@users.noreply.github.com>
2023-07-27 14:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
+    // menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 };
 
